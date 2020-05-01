610f9de000157adb988fddd8581bd8f09afd3a07
JV-Amorim <joaovitoramorimx@gmail.com>
2020-05-01 11:11:21 -0300

    Cleaning all the code.

--- Startup.cs
+++ Startup.cs
@@ -46,7 +46,8 @@ namespace OnlineStore
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                /* The default HSTS value is 30 days. You may want to change this for production scenarios, 
+                see https://aka.ms/aspnetcore-hsts. */
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
