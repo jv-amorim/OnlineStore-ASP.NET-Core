578b920c755f18572488631d7d801a37b933e856
JV-Amorim <joaovitoramorimx@gmail.com>
2020-05-31 10:51:10 -0300

    CartTotalPriceManager class implemented (JS).

--- wwwroot/js/CustomerArea/CartPageActions.js
+++ wwwroot/js/CustomerArea/CartPageActions.js
@@ -3,19 +3,34 @@ AddEventListenersToChangeProductAmountInCart();     /* JS Hoisting. */
 function AddEventListenersToChangeProductAmountInCart() {
     const increaseButtons = document.getElementsByClassName('increase-button');
     const decreaseButtons = document.getElementsByClassName('decrease-button');
-    
+
     for (let index = 0; index < increaseButtons.length; index++)
     {
         increaseButtons[index].addEventListener("click", () => {
             const productAmountManager = new ProductAmountManager(index);
+            const cartTotalPriceManager = new CartTotalPriceManager();
             productAmountManager.ChangeProductAmountInCart('+');
+            cartTotalPriceManager.UpdateCartPrices();
         });
 
         decreaseButtons[index].addEventListener("click", () => {
             const productAmountManager = new ProductAmountManager(index);
+            const cartTotalPriceManager = new CartTotalPriceManager();
             productAmountManager.ChangeProductAmountInCart('-');
+            cartTotalPriceManager.UpdateCartPrices();
         });
     }
+}
+
+function ConvertNumericPriceToStringPrice(numericPrice) {
+    return (
+        numericPrice
+        .toLocaleString(undefined, {
+            minimumFractionDigits: 2, 
+            maximumFractionDigits: 2, 
+            currency: 'USD'
+        })
+    );
 }
 
 class ProductAmountManager {
@@ -25,7 +40,7 @@ class ProductAmountManager {
         this.amount = document.getElementsByClassName('product-amount')[productIndexInCart];
         this.unitsInStock = document.getElementsByClassName('product-unitsInStock')[productIndexInCart];
         this.unitPrice = document.getElementsByClassName('product-unitPrice')[productIndexInCart];
-        this.subtotalPrice = document.getElementsByClassName('product-totalPrice')[productIndexInCart];
+        this.productTotalPrice = document.getElementsByClassName('product-totalPrice')[productIndexInCart];
 
         this.originalAmountValue = parseInt(this.amount.value);
         this.currentAmountValue = parseInt(this.amount.value);
@@ -51,6 +66,7 @@ class ProductAmountManager {
             return false;
         }
         if (this.currentAmountValue < 1) {
+            this.SetProductTotalPrice();
             this.RemoveProductFromCart();
             return false;
         }
@@ -58,14 +74,9 @@ class ProductAmountManager {
     }
 
     SetProductTotalPrice() {
-        const newSubtotal = 
-            (this.currentAmountValue * parseFloat(this.unitPrice.value))
-            .toLocaleString(undefined, {
-                minimumFractionDigits: 2, 
-                maximumFractionDigits: 2, 
-                currency: 'USD'
-            });
-        this.subtotalPrice.innerHTML = '$' + newSubtotal;
+        const newTotalPrice = this.currentAmountValue * parseFloat(this.unitPrice.value);
+        this.productTotalPrice.setAttribute('value', newTotalPrice);
+        this.productTotalPrice.innerHTML = '$' + ConvertNumericPriceToStringPrice(newTotalPrice);
     }
 
     ShowCartErrorMessage(message) {
@@ -106,5 +117,37 @@ class ProductAmountManager {
         const url = `/Cart/RemoveProductFromCart?id=${this.productIdValue}`;
         xmlHttpRequest.open('get', url, true);
         xmlHttpRequest.send();
+    }
+}
+
+class CartTotalPriceManager {
+    constructor() {
+        this.productTotalPrices = document.getElementsByClassName('product-totalPrice');
+        this.cartSubtotalPrice = document.getElementById('cart-subtotalPrice');
+        this.cartShippingRate = document.getElementById('cart-shippingRate');
+        this.cartTotalPrice = document.getElementById('cart-totalPrice');
+
+        this.cartSubtotalPriceValue = 0;
+        this.cartShippingRateValue = 0;
+    }
+
+    UpdateCartPrices() {
+        this.UpdateSubtotalPrice();
+        this.UpdateTotalPrice();
+    }
+
+    UpdateSubtotalPrice() {
+        for (let i = 0; i < this.productTotalPrices.length; i++) {
+            const priceValue = parseFloat(this.productTotalPrices[i].getAttribute('value'));
+            this.cartSubtotalPriceValue += priceValue;
+        }
+        const subtotalprice = ConvertNumericPriceToStringPrice(this.cartSubtotalPriceValue);
+        this.cartSubtotalPrice.innerHTML = '$' + subtotalprice;
+    }
+
+    UpdateTotalPrice() {
+        const totalPrice = 
+            ConvertNumericPriceToStringPrice(this.cartSubtotalPriceValue + this.cartShippingRateValue);
+        this.cartTotalPrice.innerHTML = `<b>$${totalPrice}</b>`;
     }
 }
