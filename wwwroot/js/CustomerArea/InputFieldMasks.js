c5c2778251d2a296b81a728b5d57aaebd9b839ff
JV-Amorim <joaovitoramorimx@gmail.com>
2020-06-11 22:22:01 -0300

    Some visual aspects have been improved.

--- wwwroot/js/CustomerArea/InputFieldMasks.js
+++ wwwroot/js/CustomerArea/InputFieldMasks.js
@@ -1,5 +1,7 @@
 function ApplyMasksToInputFields() {
     $('#cep-input').mask('00.000-000');
+    $('.cpf-input-field').mask('000.000.000-00');
+    $('.phone-input-field').mask('(00) 00000-0000');
 }
 
 ApplyMasksToInputFields();
