00c5c358f776ea3737a800556a10cf6d12e3c1a2
JV-Amorim <joaovitoramorimx@gmail.com>
2020-06-16 20:25:23 -0300

    The address CRUD have been implemented.

--- Areas/Customer/Controllers/AddressController.cs
+++ Areas/Customer/Controllers/AddressController.cs
@@ -38,10 +38,13 @@ namespace OnlineStore.Controllers
         {
             if (ModelState.IsValid)
             {
-                int customerId = customerSession.GetLoggedInCustomer().Id;
+                Customer loggedInCustomer = customerSession.GetLoggedInCustomer();
 
-                address.CustomerId = customerId;
+                address.CustomerId = loggedInCustomer.Id;
                 addressRepository.Register(address);
+
+                loggedInCustomer.Addresses.Add(address);
+                customerSession.Login(loggedInCustomer);
 
                 if (redirectTo != null)
                     return LocalRedirectPermanent(redirectTo);
@@ -51,6 +54,62 @@ namespace OnlineStore.Controllers
             }
 
             return View();
+        }
+
+        public IActionResult Update(int id)
+        {
+            Address addressToUpdate = addressRepository.GetAddress(id);
+
+            Customer loggedInCustomer = customerSession.GetLoggedInCustomer();
+
+            if (addressToUpdate.CustomerId != loggedInCustomer.Id)
+                return RedirectToAction(nameof(Index));
+
+            return View(addressToUpdate);
+        }
+
+        [HttpPost]
+        public IActionResult Update([FromForm] Address address)
+        {
+            Customer loggedInCustomer = customerSession.GetLoggedInCustomer();
+
+            if (address.CustomerId != loggedInCustomer.Id)
+                return RedirectToAction(nameof(Index));
+
+            if (ModelState.IsValid)
+            {
+                addressRepository.Update(address);
+
+                Address addressInCustomerSession = loggedInCustomer.Addresses.First(a => a.Id == address.Id);  
+                loggedInCustomer.Addresses.Remove(addressInCustomerSession);
+                loggedInCustomer.Addresses.Add(address);
+
+                customerSession.Login(loggedInCustomer);
+
+                TempData["MSG_OK"] = Message.MSG_OK_002;
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [ValidateHttpReferer]
+        public IActionResult Delete(int id)
+        {
+            Customer loggedInCustomer = customerSession.GetLoggedInCustomer();
+            
+            Address addressToDelete = addressRepository.GetAddress(id);
+            if (addressToDelete.CustomerId != loggedInCustomer.Id)
+                return RedirectToAction(nameof(Index));
+
+            Address addressInCustomerSession = loggedInCustomer.Addresses.First(a => a.Id == id);
+            loggedInCustomer.Addresses.Remove(addressInCustomerSession);
+            customerSession.Login(loggedInCustomer);
+
+            addressRepository.Delete(id);
+
+            TempData["MSG_OK"] = Message.MSG_OK_003;
+            return RedirectToAction(nameof(Index));
         }
     }
 }
